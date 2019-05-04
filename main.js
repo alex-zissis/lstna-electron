e4ce11d18d891d841e935e7b61ee96cb33a52b7c
Alex Zissis <alex@alexzissis.tech>
2019-05-04 17:59:30 +1000

    boilerplate

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+require('dotenv').config();
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    backgroundColor: '#ffffff',
+    icon: `file://${__dirname}/dist/listen-for-apple-music/assets/logo.png`
+  });
+
+  if (process.env.NODE_ENV !== 'prod') {
+    win.webContents.toggleDevTools();
+  }
+
+
+  win.loadURL(`file://${__dirname}/dist/listen-for-apple-music/index.html`);
+
+  // Event when the window is closed.
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+// Create window on electron intialization
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // macOS specific close process
+  if (win === null) {
+    createWindow();
+  }
+});
